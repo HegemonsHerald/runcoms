b6fd4846dd0a35f9aa285d72206bfcaf6496e041
RevMatres <benediktheinen1@gmail.com>
2018-10-11 22:35:48 +0200

    rewrote the wacom toggle touch script to take as argument 'on' and 'off' and toggle/set for all touch devices

--- .scripts/wacom-toggle_touch.sh
+++ .scripts/wacom-toggle_touch.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-# the wacom touch device to toggle
-touch="Wacom ISDv5 326 Finger touch"
 
-# get the current status of the device
-state="$(xsetwacom --get "$touch" Touch)"
+# first get the touch devices			    | for each of those...
+xsetwacom --list devices | ag --nocolor "touch|pad" | while read touch; do
 
-echo -e "intial status: $state\n"
+	# get the device id
+	#				remove everything after the device name (since there's no '^' this will only match against any number of spaces RIGHT BEFORE 'id' and then the rest of the line) and not also against everything before the spaces...
+	touch=$( echo $touch | sed -e 's/[[:space:]]*id.*$//' )
+	# Learn from this: You can match against parts of lines, you don't HAVE to match against the whole line
+	# Note: I'm using echo cause I need to have a \n for matching against the $
 
-# if Touch is enabled, disable it, else enable it
-test on == $state && ( xsetwacom --set "$touch" Touch off; echo -e "Touch disabled\nfinal status: off" ) || ( xsetwacom --set "$touch" Touch on; echo -e "Touch enabled\nfinal status: on" )
+	# get the state
+	state="$(xsetwacom --get "$touch" Touch)"
 
+	# print some stuff
+	printf "device: $touch\n  intial status: $state\n"
+
+	# if input args have been provided
+	if [ "$1" = "on" ]; then
+		xsetwacom --set "$touch" Touch on; printf "  Touch enabled\n  final status: on\n"
+	elif [ "$1" = "off" ]; then
+		xsetwacom --set "$touch" Touch off; printf "  Touch disabled\n  final status: off\n"
+	else
+		# toggle the state
+		test on == $state && ( xsetwacom --set "$touch" Touch off; printf "  Touch disabled\n  final status: off\n" ) || ( xsetwacom --set "$touch" Touch on; printf "  Touch enabled\n  final status: on\n" )
+	fi
+done
